bece22354d95af89c35f7537a173e3f7a8ed53b9
tswinnie <tyrone.swinnie@gmail.com>
2014-08-11 11:29:17 -0400

    cleaned up
    cleaned up site and added validation for form on site

--- logicdesign/js/main.js
+++ logicdesign/js/main.js
@@ -1,63 +1,63 @@
-/**
- * Created by tyroneswinnie on 6/25/14.
- */
-
-window.onload=function(){
-
-var newContainer = document.querySelector("#container");
-
-    newContainer.style.width = 1024;
-    newContainer.style.marginLeft = "45%";
-    var newLabel = document.querySelectorAll("label");
-    for(var i = 0; i < newLabel.length; i++){
-        newLabel[i].style.width = 400;
-        newLabel[i].style.display = "block";
-        newLabel[i].style.color = "#282828";
-        newLabel[i].style.font = "Normal small-caps 300 18px/150% Helvetica Neue, sans-serif";
-
-
-
-
-
-    }
-
-    var newInputs = document.querySelectorAll("input");
-
-    for(var j = 0; j < newInputs.length; j++){
-
-        newInputs[j].style.border = "1px solid green";
-        newInputs[j].style.borderWidth = "220";
-
-
-
-    }
-
-
+///**
+// * Created by tyroneswinnie on 6/25/14.
+// */
 //
-
-
-
-
-};
-
-
-
-    $(function() {
-
-        var fonts = ['Logical', 'Designer', 'Mobile Developer', 'UX Developer'];
-        var time = setInterval(function() {
-            var newFont = fonts[Math.floor(Math.random()*fonts.length)];
-            $('.whoamIOne').fadeOut();
-
-            $('.whoamITwo').fadeIn(100).text(newFont).animate({ top: '56%' }, 4000, 'easeOutElastic');
-        },2500);
-
-    });
-
-
-
-
-
-
-
-
+//window.onload=function(){
+//
+//var newContainer = document.querySelector("#container");
+//
+//    newContainer.style.width = 1024;
+//    newContainer.style.marginLeft = "45%";
+//    var newLabel = document.querySelectorAll("label");
+//    for(var i = 0; i < newLabel.length; i++){
+//        newLabel[i].style.width = 400;
+//        newLabel[i].style.display = "block";
+//        newLabel[i].style.color = "#282828";
+//        newLabel[i].style.font = "Normal small-caps 300 18px/150% Helvetica Neue, sans-serif";
+//
+//
+//
+//
+//
+//    }
+//
+//    var newInputs = document.querySelectorAll("input");
+//
+//    for(var j = 0; j < newInputs.length; j++){
+//
+//        newInputs[j].style.border = "1px solid green";
+//        newInputs[j].style.borderWidth = "220";
+//
+//
+//
+//    }
+//
+//
+////
+//
+//
+//
+//
+//};
+//
+//
+//
+//    $(function() {
+//
+//        var fonts = ['Logical', 'Designer', 'Mobile Developer', 'UX Developer'];
+//        var time = setInterval(function() {
+//            var newFont = fonts[Math.floor(Math.random()*fonts.length)];
+//            $('.whoamIOne').fadeOut();
+//
+//            $('.whoamITwo').fadeIn(100).text(newFont).animate({ top: '56%' }, 4000, 'easeOutElastic');
+//        },2500);
+//
+//    });
+//
+//
+//
+//
+//
+//
+//
+//
